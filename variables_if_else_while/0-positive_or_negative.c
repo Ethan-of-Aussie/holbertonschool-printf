591e4746233d44e4aacb82d8ce51d5bbf6138c7b
Sijin35 <sijinsingh222@gmail.com>
2026-02-11 07:30:41 +0000

    Program for checking +ve or -ve

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -  printing numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("positive \n");
+	else if (n == 0)
+		printf("zero \n");
+	else
+		printf("negative \n");
+	return (0);
+}
