0f894e10a5f1702b179f3756f18a19df0d606496
Sijin35 <sijinsingh222@gmail.com>
2026-02-14 10:04:55 +0000

    Program to display all numbers if base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 16 in lowercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
