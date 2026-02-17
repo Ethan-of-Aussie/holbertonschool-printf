1a3816f3018eb3fa5e31331acb351523c44534e1
Sijin35 <sijinsingh222@gmail.com>
2026-02-17 01:36:20 +0000

    HAppy Betty :)

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints numbers 10x
+ * more_numbers - Prints numbers 10x
  *
  * Return: Always Success
  */
@@ -11,7 +11,6 @@ void more_numbers(void)
 	int i;
 	int a;
 
-	
 	for (i = 1; i < 11; i++)
 	{
 		for (a = 0; a <= 14; a++)
@@ -20,9 +19,7 @@ void more_numbers(void)
 			{
 				_putchar('1');
 			}
-
 			_putchar('0' + (a % 10));
-			
 		}
 		_putchar('\n');
 	}
