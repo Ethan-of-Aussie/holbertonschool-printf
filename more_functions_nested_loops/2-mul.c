78b46b12bf1749de6b1714ba6bf9cb9e58c461ea
Sijin35 <sijinsingh222@gmail.com>
2026-02-17 00:09:28 +0000

    Created files to write a funbction to multiply two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - a * b
+ *
+ * @a: 1st value
+ *
+ * @b: 2nd value
+ *
+ * Return: Always Success
+ */
+
+int mul(int a, int b)
+{
+	int result; 
+
+	result = a * b;
+
+	return (result);
+}
