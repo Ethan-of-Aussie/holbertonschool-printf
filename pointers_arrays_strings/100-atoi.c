04a7abb29ced6833d1ad7ce1fb2e8e47b1834b3b
Sijin35 <sijinsingh222@gmail.com>
2026-02-22 09:52:29 +0000

    Fixed issues related to overflow

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Converts string to integer
@@ -10,7 +11,8 @@
 
 int _atoi(char *s)
 {
-	int i = 0, result = 0, sign = 1, start = 0;
+	int i = 0, sign = 1, start = 0;
+	long result = 0;
 
 	for (; s[i] != '\0'; i++)
 	{
@@ -22,6 +24,15 @@ int _atoi(char *s)
 		{
 			start = 1;
 			result = result * 10 + (s[i] - '0');
+			
+			if (sign == 1 && result > INT_MAX)
+			{
+				return (INT_MAX);
+			}
+			if (sign == -1 && result < INT_MIN)
+			{
+				return (INT_MIN);
+			}
 		}
 		else if(start == 1)
 		{
@@ -29,5 +40,5 @@ int _atoi(char *s)
 		}
 	}
 
-	return (result * sign);
+	return (int)(result * sign);
 }
