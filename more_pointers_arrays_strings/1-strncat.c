6a2a3310b1782f5187afd2d46e959cbaed77699e
Sijin35 <sijinsingh222@gmail.com>
2026-02-23 10:59:02 +0000

    Function that concatenates based on 'n' amount of characters

--- more_pointers_arrays_strings/1-strncat.c
+++ more_pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,31 @@
 #include "main.h"
 
+/**
+ * *_strncat: Concatenates two strings
+ *
+ * @dest: Targer string
+ *
+ * @src: To be joined to target
+ *
+ * @n: specified bytes
+ *
+ * Return: Concatenated string
+ */
 
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j <  n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
