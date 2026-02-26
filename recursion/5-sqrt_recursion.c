4435c173a1a08c05c0f5f861f098a5dc062be4b1
Sijin35 <sijinsingh222@gmaill.com>
2026-02-26 03:42:36 +0000

    Function that calculates the square root (draft with only logic, which possibly might be wrong, need to check)

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ *
+ * @n: number whose square root is to be calculated
+ *
+ * Return: The calculated square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n ^ (1 / 2) % != 0)
+	{
+		return (-1);
+	}
+
+	return (n ^ (1 / 2));
+}
