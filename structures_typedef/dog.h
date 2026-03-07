f6d26ecd1a01d673f8a371d40732220339fc0586
Sijin35 <sijinsingh222@gmail.com>
2026-03-07 08:33:14 +0000

    New type struct dog with relevant elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - Structure for dog
+ *
+ * @name: Name of dog
+ *
+ * @age: Age of dog
+ *
+ * @owner: Owner of dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
