2fa664dd2370cf3f79043ec5e2826c88c7d35694
Sijin35 <sijinsingh222@gmail.com>
2026-03-08 09:27:15 +0000

    Removed _putchar prototype from main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,20 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 #endif
