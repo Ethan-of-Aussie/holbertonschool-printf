80d792009dd4a62277a9510dd490effef48d1de5
Sijin35 <sijinsingh222@gmail.com>
2026-03-13 09:48:25 +0000

    Fixed swith cases

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,15 +12,13 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0, integer;
-	char *separator = "";
+	char *separator = "", *str;
 	char c;
 	double f;
-	char *str;
 
 	va_start(args, format);
 	if (format == NULL)
-	{	return;
-	}
+	{	return; }
 	while (format[i] != '\0')
 	{
 		switch (format[i])
@@ -28,24 +26,26 @@ void print_all(const char * const format, ...)
 			case 'c':
 				c = (char)va_arg(args, int);
 				printf("%s%c", separator, c);
+				separator = ", ";
 				break;
 			case 'i':
 				integer = va_arg(args, int);
 				printf("%s%d", separator, integer);
+				separator = ", ";
 				break;
 			case 'f':
 				f = (double)va_arg(args, double);
 				printf("%s%f", separator, f);
+				separator = ", ";
 				break;
 			case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
-				{	str = "(nil)";
-				}
+				{	str = "(nil)";	}
 				printf("%s%s", separator, str);
+				separator = ", ";
 				break;
 		}
-		separator = ", ";
 		i++;
 	}
 	printf("\n");
