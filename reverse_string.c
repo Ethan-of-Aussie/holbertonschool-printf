be3070fa1ea0cf0be0c2798d1c82dc20356b333c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-03-18 16:23:10 +1100

    Update reverse_string.c
    fixed indent

--- reverse_string.c
+++ reverse_string.c
@@ -7,16 +7,16 @@
  */
 void reverse_string(char *str, int length)
 {
-  int start = 0;
-  int end = length - 1;
-  char temp;
-  
-  while (start < end)
+    int start = 0;
+    int end = length - 1;
+    char temp;
+
+    while (start < end)
     {
-      temp = str[start];
-      str[start] = str[end];
-      str[end] = temp;
-      start++;
-      end--;
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
     }
 }
